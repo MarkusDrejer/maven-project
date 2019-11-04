85fca0f27fd5f5e624f1972224f94ed796d11d9d
MarkusDrejer <mabd412b@stud.kea.dk>
2019-11-04 09:21:20 +0100

    Yes x2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,9 @@ public class Greeter {
   /** This method is used to greet the user using a final String as a parameter.
    * Then returns "Hello" with that String following.
    * @param someone
-   * @return
+   * A parameter indicating the name of the user to greet
+   * @return String
+   * Combines Hello + Someone
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
